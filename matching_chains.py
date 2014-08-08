3dbbc46e7c08ed618515902b213c493f52976b98
Martin Chorley <m.j.chorley@cs.cardiff.ac.uk>
2014-08-08 11:31:15 +0100

    first bash at chain matching

--- matching_chains.py
+++ matching_chains.py
@@ -1,5 +1,6 @@
 import os
 import json
+import uuid
 
 from Levenshtein import ratio
 from collections import defaultdict
@@ -13,14 +14,56 @@ def match_chains(venues_data):
     id_lookup = defaultdict(set)
 
     combos = combinations(venues_data, 2)
-    print dir(combos)
+
+    for i, combo in enumerate(combos):
+
+        print i
+        
+        v1 = combo[0]
+        v2 = combo[1]
+
+        name_distance = ratio(v1['name'], v2['name'])
+        if v1['url'] or v2['url']:
+            url_distance = ratio(v1['url'], v2['url'])
+        else:
+            url_distance = 0
+        if v1['twitter'] or v2['twitter']:
+            twitter_distance = ratio(v1['twitter'], v2['twitter'])
+        else:
+            twitter_distance = 0
+
+        total_distance = name_distance + url_distance + twitter_distance
+
+        if total_distance > 0.9:
+
+            if chain_lookup.get(v1['foursq_id'], False):
+                chain_id = chain_lookup[v1['foursq_id']]
+            elif chain_lookup.get(v2['foursq_id'], False):
+                chain_id = chain_lookup[v2['foursq_id']]
+            else:
+                chain_id = uuid.uuid4().hex
+
+            chain_lookup[v1['foursq_id']] = chain_id
+            chain_lookup[v2['foursq_id']] = chain_id
+
+            id_lookup[chain_id].add(v1['foursq_id'])
+            id_lookup[chain_id].add(v2['foursq_id'])
+
+    return chain_lookup, id_lookup
+
 
 if __name__ == '__main__':
 
     # store and load data here
     data_dir = os.path.join(os.getcwd(), 'data')
 
-    venues_list = json.load(os.path.join(data_dir, 'venue_data_list.json'))
+    venues_list = json.load(open(os.path.join(data_dir, 'venue_data_list.json')))
 
-    match_chains(venues_list)
+    chain_lookup, id_lookup = match_chains(venues_list)
+
+    with open(os.path.join(data_dir, 'chain_lookup.json'), 'w') as out_file:
+        json.dump(chain_lookup, out_file)
+
+    with open(os.path.join(data_dir, 'id_lookup.json'), 'w') as out_file:
+        json.dump(id_lookup, out_file)
 
